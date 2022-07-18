500f6f64445e994123918309202ba16e03aedb52
fachri <108580447+Fachri31@users.noreply.github.com>
2022-07-18 10:59:50 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -158,9 +158,9 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     conn.sendHydrated(m.chat, text.trim(), 'Ⓟ premium | Ⓛ limit', null, 'https://aiinne.github.io/', 'Website', '', '', [
-      ['Donate', '/donasi'],
-      ['Sewa Bot', '/sewa'],
-      ['Owner', '/owner']
+      ['Donate🤖', '/donasi🤖'],
+      ['Sewa Bot💬', '/sewa💬'],
+      ['Owner🗿, '/owner🗿']
     ], m)
     /*let url = `https://telegra.ph/file/ab1df70dfd5c2bac64da1.jpg`.trim()
     let res = await fetch(url)
