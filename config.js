47f025387c6b978a76897c765a9bfdaf7a8d7707
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-20 17:13:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6288227758096'],
-  ['6288227758096'],
-  ['6288227758096', 'Owner', true]
+  ['6281322577123'],
+  ['6281322577123'],
+  ['6281322577123', 'Owner Lintang', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@FazhBoT'
-  var sticker_author = 'FazhBoT'
+  var sticker_name = '@Sad Botz'
+  var sticker_author = 'Lintang'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Fachri'
+global.wm = '© Sad Botz - Lintang'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
